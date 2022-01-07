0f206e5b0ba2aa270d1d2018323dd8e960469194
maoxin53 <maoxin53@gmail.com>
2022-01-08 03:13:11 +0800

    linear progress bar

--- lib/leetCodeChart.dart
+++ lib/leetCodeChart.dart
@@ -1,4 +1,33 @@
 import 'package:flutter/material.dart';
 import 'package:percent_indicator/percent_indicator.dart';
 
-class LeetCodeChart extends StatelessWidget {}
+class LeetCodeChart extends StatelessWidget {
+  var progress;
+
+  LeetCodeChart(this.progress);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      margin: EdgeInsets.all(20),
+      color: Color.fromRGBO(200, 200, 200, 0.0),
+      child: LinearPercentIndicator(
+        width: MediaQuery.of(context).size.width - 50,
+        animation: true,
+        lineHeight: 20.0,
+        animationDuration: 2000,
+        linearStrokeCap: LinearStrokeCap.roundAll,
+        progressColor: Colors.greenAccent,
+        percent: progress,
+        backgroundColor: Colors.white,
+        center: Text(
+          (progress * 100).toStringAsPrecision(3) + '%',
+          style: TextStyle(
+              fontWeight: FontWeight.bold, fontSize: 25.0, color: Colors.white),
+        ),
+        //progressColor: Color.fromRGBO(0, 153, 204, 0.7),
+      ),
+    );
+  }
+}
